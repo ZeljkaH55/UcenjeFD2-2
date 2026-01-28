9b082edb1194a4524143c97ffb06d7ea30a73521
ZeljkaH55 <zeljkaharamina70@gmail.com>
2026-01-28 18:54:32 +0100

    javascript01

---
+++ 2_JS/vjezba01/skripta.js
@@ -0,0 +1,22 @@
+console.info('Hallo world 6');
+console.table({ime:"Pero",prezime:"Perić"});
+
+document.getElementById('ispis').innerText='Hallo world 7';
+document.getElementById('ispis').style.color='red';
+
+document.getElementById('ue').innerHTML='<a href="#">Hello 8</a>';
+
+console.log('Osnovna škole "Ivana Filipovića"');
+
+console.log('MC\'Donalds');
+console.log("Mc'Donalds");
+
+console.log('radim \\')
+
+console.log(`
+    ovo je 
+    tekst
+    kroz više redova
+    `);
+
+    
