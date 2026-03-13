92d82b24f22c8e2c382913a0c1e11624bb4af0cb
ZeljkaH55 <zeljkaharamina70@gmail.com>
2026-03-13 19:44:50 +0100

    promjene

--- 3_REACT/EdunovaAPP/src/App.jsx
+++ 3_REACT/EdunovaAPP/src/App.jsx
@@ -1,15 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import {Container} from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import Izbornik from './components/Izbornik'
+import SmjerPregled from './pages/smjerovi/SmjerPregled'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import { RouteNames } from './constants'
 
 function App() {
- 
 
   return (
-  <Container>
-    <Izbornik />
-  </Container>
+    <Container>
+      <Izbornik />
+        <Routes>
+          <Route path={RouteNames.HOME} element={<Home />} />
+          <Route path={RouteNames.SMJEROVI} element={<SmjerPregled />} />
+
+        </Routes>
+      <hr />
+      &copy; Edunova
+    </Container>
   )
 }
 
---
+++ 3_REACT/EdunovaAPP/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { IME_APLIKACIJE } from "../constants";
+
+export default function Home() {
+    return(
+        <>
+        <h1>Dobrodošli u {IME_APLIKACIJE}</h1>
+        <div>
+            Slika
+        </div>
+        </>
+
+    )
+}
