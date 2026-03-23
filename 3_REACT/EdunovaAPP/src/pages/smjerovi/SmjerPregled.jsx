3e70c41346da822b959fbd6ffe4073784de5a501
ZeljkaH55 <zeljkaharamina70@gmail.com>
2026-03-23 20:50:48 +0100

    Priprema za dodavanje novog smjera

--- 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
+++ 3_REACT/EdunovaAPP/src/pages/smjerovi/SmjerPregled.jsx
@@ -4,6 +4,8 @@ import { Table } from "react-bootstrap"
 import { NumericFormat } from "react-number-format"
 import { GrValidate } from "react-icons/gr"
 import FormatDatuma from "../../components/FormatDatuma"
+import { Link } from "react-router-dom"
+import { RouteNames } from "../../constants"
 
 export default function SmjerPregled(){
 
@@ -21,6 +23,13 @@ export default function SmjerPregled(){
 
     return(
         <>
+
+        <Link to ={RouteNames.SMJEROVI_NOVI}
+        className="btn btn-success">
+            Dodavanje novog smjera
+        </Link>
+
+
         <Table>
           <thead>
             <tr>
